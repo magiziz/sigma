5a609ec0ca8874952efb74c31c18261f2e3d20dc
Mago Khamidov <mago.khamidov@gmail.com>
2022-09-02 12:03:39 +0100

    Added all the basics react router dom, tailwind, setted up components and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"min-h-screen bg-pink"}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<h1>Hey</h1>} />
+        <Route
+          path="*"
+          element={
+            <h1 className={"w-full text-center mt-3"}>Page not found sorry</h1>
+          }
+        />
+      </Routes>
     </div>
   );
 }
